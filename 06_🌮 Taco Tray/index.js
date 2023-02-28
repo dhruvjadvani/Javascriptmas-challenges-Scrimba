06033b198cead01137f5b70db952317c4ad9a575
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2023-03-01 01:56:18 +0530

    Update index.js

--- 06_🌮 Taco Tray/index.js
+++ 06_🌮 Taco Tray/index.js
@@ -1,1 +1,37 @@
+// Review Challenge 4: Taco Tray
+// - Help our chef fill a tray with tacos!
+
+function getRandomNumberOfTacos() {
+  /*
+    Make this function return an array that contains 
+    between one and ten taco emojis 🌮
+    Use the following JavaScript concepts:
+        - Math.random()
+        - Math.floor()
+        - new Array()
+        - Array.fill()
+    */
+
+  const randomNum = Math.floor(Math.random() * 10);
+  const taco = "🌮";
+
+  let newArr = [];
+
+  for (let i = 0; i < randomNum; i++) {
+    newArr.push(taco);
+  }
+
+  return newArr; 
+}
+
+
+function putTacosOnTray() {
+  return getRandomNumberOfTacos()
+    .map(function (taco) {
+      return `<div class="taco">${taco}</div>`;
+    })
+    .join("");
+}
+
+document.getElementById("tray").innerHTML = putTacosOnTray();
 
