1a20558b1ca785ab7878cf25e9d5c499e77a2462
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2023-03-03 08:15:37 +0530

    Update index.js

--- 14_Count Vowel Consonant/index.js
+++ 14_Count Vowel Consonant/index.js
@@ -1,1 +1,45 @@
+function countVowelConsonant(str) {
+  // write code here
+  let newStrArr = str.split("");
+  let count = 0;
+  for(let i = 0; i < newStrArr.length; i++) {
+    //   console.log(newStrArr[i]);
+      if(newStrArr[i] === "a") {
+            count++;
+      } else if(newStrArr[i] === "e") {
+          count++;
+      } else if(newStrArr[i] === "i") {
+          count++;
+      } else if(newStrArr[i] === "o") {
+          count++;
+      } else if(newStrArr[i] === "u") {
+          count++;
+      } else {
+          count = count + 2;
+      }
+  }
+  return count;
+}
 
+
+
+/**
+* Test Suite 
+*/
+describe('countVowelConsonant()', () => {
+    it('returns total of vowels(1) and consonants(2) to be added', () => {
+        // arrange
+        const value = 'abcde';
+        
+        // act
+        const result = countVowelConsonant(value);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toBe(8);
+    });
+});
+
+console.log(countVowelConsonant("abcde"));
