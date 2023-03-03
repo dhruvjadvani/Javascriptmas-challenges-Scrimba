e610dc1150aa80f24fd641cf1213f6ce40285c35
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2023-03-03 23:17:11 +0530

    Update index.js

--- 16_Insert Dashes/index.js
+++ 16_Insert Dashes/index.js
@@ -1,1 +1,31 @@
+function insertDashes(arr) {
+    // write code here
+    let newArr = arr.split(" ");
+    
+    const resArr = newArr.map(val => val.split("").join("-"));
+    
+    const resStr = resArr.join(" ");
+    
+    return resStr;
+}
 
+
+console.log(insertDashes("aba aba"));
+/**
+* Test Suite 
+*/
+describe('insertDashes()', () => {
+    it('insert dashes in between chars', () => {
+        // arrange
+        const value = "aba caba";
+        
+        // act
+        const result = insertDashes(value);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toBe("a-b-a c-a-b-a");
+    });
+});
