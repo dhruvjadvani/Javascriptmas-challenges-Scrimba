ddf11ffd6d87cc4346829b99fb0f62bd2781db24
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2023-03-04 11:50:54 +0530

    Update index.js

--- 22_🍴 Ready Tables/index.js
+++ 22_🍴 Ready Tables/index.js
@@ -1,1 +1,44 @@
+function getReadyTables() {
+  const readyTables = [];
+  for (let i = 0; i < 2; i++) {
+    readyTables.push(Math.round(Math.random() * 20) + 1);
+  }
+  // console.log(readyTables);
+  return readyTables;
+}
+function displayTables() {
+  const arr = getReadyTables();
+
+  const res = arr
+    .map((val) => {
+      return `<div class="table">${val}</div>`;
+    })
+    .join("");
+
+  return res;
+}
+
+const table = document.getElementById("tables");
+
+table.innerHTML = displayTables();
+
+/* Challenge 3: Ready Tables
+- Topic: Return a Function
+Our restaurant has 20 tables and the staff serves 
+2 tables at a time. They need to know which tables 
+are ready to serve.  
+Let's fix this with what we learned about returning a 
+function inside of another function.
+
+1. Create a function called displayTables.
+2. displayTables should map over the array of ready tables 
+returned from getReadyTables to generate the html we 
+need to render our display for the staff. One div should 
+be generated for each table value. Here is an example: 
+`<div class="table">TABLE VALUE HERE</div>`. 
+3. Remember to remove the commas after applying map()
+4. Fetch the tables <section> from the DOM 
+5. Set the innerHTML content of the tables <section> 
+to the displayTables function call.
+*/
 
