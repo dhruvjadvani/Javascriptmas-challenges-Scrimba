f2a1ceb9904becd4527aa09aa8c8c2e3f185e222
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2023-03-04 11:02:34 +0530

    Update index.js

--- 19_Century From Year/index.js
+++ 19_Century From Year/index.js
@@ -1,1 +1,46 @@
+function centuryFromYear(num) {
+    //  write code here.
+    const year = num;
+    
+    const century = year % 100 === 0 ? year / 100 : Math.floor(year / 100) + 1;
+    
+    return century;
+}
 
+
+
+/**
+* Test Suite 
+*/
+describe('centuryFromYear()', () => {
+    it('returns current century', () => {
+        // arrange
+        const year = 1905;
+        
+        // act
+        const result = centuryFromYear(year);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(20);
+    });
+    
+    it('returns current century for edge case of start of century', () => {
+        // arrange
+        const year = 1700;
+        
+        // act
+        const result = centuryFromYear(year);
+
+        // log
+        console.log("result 2: ", result);
+        
+        // assert
+        expect(result).toBe(17);
+    });    
+});
+
+
+console.log(centuryFromYear(2023));
