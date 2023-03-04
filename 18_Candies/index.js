547e8c0eb6678d4a03be44a4d40183cdd2722630
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2023-03-04 10:55:26 +0530

    Update index.js

--- 18_Candies/index.js
+++ 18_Candies/index.js
@@ -1,1 +1,33 @@
+function candies(children, candy) {
+    //  write code here.
+    const candiesPerChild = Math.floor(candy / children);
+    const totalChildren = children;
+    // console.log(candiesPerChild);
+    const maxCandies = candiesPerChild * totalChildren;
+    
+    return maxCandies;
+}
 
+
+
+/**
+* Test Suite 
+*/
+describe('candies()', () => {
+    it('returns ammount of total equal candy to be eaten', () => {
+        // arrange
+        const children = 3;
+        const candy = 10;
+        
+        // act
+        const result = candies(children, candy);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toBe(9);
+    });
+});
+
+console.log(candies(3, 10));
