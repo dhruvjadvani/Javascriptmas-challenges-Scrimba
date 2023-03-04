efd809aa76d7251fcabdead58e26f169f15a4cc9
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2023-03-04 21:05:58 +0530

    Update index.js

--- 10_Sort by Length (Introduction)/index.js
+++ 10_Sort by Length (Introduction)/index.js
@@ -1,1 +1,44 @@
+function sortByLength(strs) {
+    //  write code here.
+    
+    let arr = [...strs];
+    
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = i + 1; j < arr.length; j++) {
+            let pointer1 = i;
+            let pointer2 = j;
+            
+            
+            if(arr[pointer1].length > arr[pointer2].length) {
+                let temp = arr[pointer1];
+                arr[pointer1] = arr[pointer2];
+                arr[pointer2] = temp;
+                
+            }
+        }
+    }
+    
+    return arr;
+}
 
+console.log(sortByLength(["abc", "", "aaa", "a", "zz"]));
+
+
+/**
+* Test Suite 
+*/
+describe('sortByLength()', () => {
+    it('sorts the strings from shortest to longest', () => {
+        // arrange
+        const strs = ["abc", "", "aaa", "a", "zz"];
+        
+        // act
+        const result = sortByLength(strs);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toEqual(["", "a", "zz", "abc", "aaa"]);
+    });
+});
