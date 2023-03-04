8613f39a46cb6a56e19e1deb3a71cdccf0181618
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2023-03-04 22:04:07 +0530

    Update index.js

--- 24_🎶 Christmas Jukebox/index.js
+++ 24_🎶 Christmas Jukebox/index.js
@@ -1,1 +1,7 @@
+const player = document.getElementById("player");
+
+function playSong(id) {
+  // Challenge: Add code here to make the youtube player play the new YouTube song
+  player.src = `https://www.youtube.com/embed/${id}`;
+}
 
